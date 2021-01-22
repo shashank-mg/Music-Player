d896210023d5194050858870bf35a6f3ef0c6290
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2021-01-22 21:44:35 +0530

    music player app

---
+++ app.js
@@ -0,0 +1,6 @@
+var theme = new Howl({
+  src: ["theme.mp3"],
+  autoplay: true,
+  loop: true,
+});
+theme.play();
---
+++ elements.js
@@ -0,0 +1,3 @@
+const play_pause = document.querySelector(".play");
+const play_prev = document.querySelector(".prev");
+const play_next = document.querySelector(".next");
