71f38445359d9b83a85e838d0289c6dd5853a5e6
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2021-01-23 21:53:11 +0530

    play pause resume

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
-var theme = new Howl({
-  src: ["theme.mp3"],
-  autoplay: true,
-  loop: true,
-});
-theme.play();
+class MediaPlayer {
+  constructor() {
+    this.tracker = 0;
+    this.playing = 0;
+    this.getId = 0;
+    this.path = songs[this.tracker];
+  }
+
+  loadTrack = (path) => {
+    if (~this.getID) {
+      this.tracksPlaying = new Howl({
+        src: [`./playlist/${this.path}`],
+      });
+      console.log(this.tracksPlaying);
+      return this.tracksPlaying;
+    } else {
+      return this.tracksPlaying;
+    }
+  };
+
+  playTheSong = () => {
+    if (this.playing) {
+      this.pauseTheSong(this.getID);
+      this.playing = 0;
+      console.log(this.getId);
+    } else if (this.playing === 0) {
+      if (~this.getID) {
+        this.getID = this.loadTrack(this.path).play();
+        console.log(this.getId);
+      } else {
+        console.log(this.getID);
+        this.resumePlaying(this.getID);
+      }
+      this.playing = 1;
+    }
+  };
+
+  resumePlaying = (id) => this.tracksPlaying.playing(id);
+
+  stopTheSong = (id) => this.tracksPlaying.stop(id);
+
+  playTheNextSong = () => {
+    if (this.tracker < songs.length) {
+      this.tracker++;
+      this.stopTheSong(this.getID);
+    }
+    this.path = songs[this.tracker];
+    this.playing = 0;
+    this.getID = 0;
+    this.playTheSong();
+  };
+
+  playThePrevSong = () => {
+    if (this.tracker > 0) this.tracker--;
+    this.songList.play();
+  };
+
+  pauseTheSong(id) {
+    this.tracksPlaying.pause(id);
+  }
+}
+
+const media_player = new MediaPlayer();
--- elements.js
+++ elements.js
@@ -1,3 +1,22 @@
+let songs = [
+  "Boyfriend.mp3",
+  "Catching-Feelings.mp3",
+  "Cold-Water.mp3",
+  "Forever.mp3",
+  "Freeze.mp3",
+  "I-Would.mp3",
+  "Out-Of-Town-Girl.mp3",
+  "Take-You.mp3",
+  "What-Do-You-Mean.mp3",
+  "Yellow-Raincoat.mp3",
+];
+
 const play_pause = document.querySelector(".play");
 const play_prev = document.querySelector(".prev");
 const play_next = document.querySelector(".next");
+
+play_pause.addEventListener("click", () => media_player.playTheSong());
+
+play_prev.addEventListener("click", () => {});
+
+play_next.addEventListener("click", () => media_player.playTheNextSong());
