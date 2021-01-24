4358696a2da9aad34aa6bafc2042033377eb3918
shashank m.g <52480486+shashank-mg@users.noreply.github.com>
2021-01-24 21:20:08 +0530

    duration to be corrected

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ class MediaPlayer {
   constructor() {
     this.tracker = 0;
     this.playing = 0;
-    this.getId = 0;
-    this.path = songs[this.tracker];
+    this.getId = false;
+    this.seek = null;
+    this.artist = songs[this.tracker][1];
+    this.path = songs[this.tracker][0];
   }
 
   loadTrack = (path) => {
     if (~this.getID) {
       this.tracksPlaying = new Howl({
-        src: [`./playlist/${this.path}`],
+        src: [`./playlist/${path}`],
       });
       console.log(this.tracksPlaying);
       return this.tracksPlaying;
@@ -22,42 +24,50 @@ class MediaPlayer {
     if (this.playing) {
       this.pauseTheSong(this.getID);
       this.playing = 0;
-      console.log(this.getId);
     } else if (this.playing === 0) {
-      if (~this.getID) {
-        this.getID = this.loadTrack(this.path).play();
-        console.log(this.getId);
-      } else {
-        console.log(this.getID);
-        this.resumePlaying(this.getID);
-      }
+      if (!this.getID) this.getID = this.loadTrack(this.path).play();
+      else this.resumePlaying(this.getID);
       this.playing = 1;
     }
+    let duration = this.tracksPlaying._sounds[0];
+    displayName(this.path, duration._parent._duration, this.artist);
   };
 
-  resumePlaying = (id) => this.tracksPlaying.playing(id);
+  resumePlaying = (id) => {
+    this.tracksPlaying.play(id);
+    this.tracksPlaying.seek(this.seek, id);
+  };
 
   stopTheSong = (id) => this.tracksPlaying.stop(id);
 
   playTheNextSong = () => {
-    if (this.tracker < songs.length) {
+    if (this.tracker < songs.length - 1) {
       this.tracker++;
       this.stopTheSong(this.getID);
+      this.path = songs[this.tracker][0];
+      this.artist = songs[this.tracker][1];
+      this.playing = 0;
+      this.getID = undefined;
+      this.playTheSong();
     }
-    this.path = songs[this.tracker];
-    this.playing = 0;
-    this.getID = 0;
-    this.playTheSong();
   };
 
   playThePrevSong = () => {
-    if (this.tracker > 0) this.tracker--;
-    this.songList.play();
+    if (this.tracker > 0) {
+      this.tracker--;
+      this.stopTheSong(this.getID);
+      this.path = songs[this.tracker][0];
+      this.artist = songs[this.tracker][1];
+      this.playing = 0;
+      this.getID = undefined;
+      this.playTheSong();
+    }
   };
 
-  pauseTheSong(id) {
-    this.tracksPlaying.pause(id);
-  }
+  pauseTheSong = (id) => {
+    this.tracksPlaying.pause();
+    this.seek = this.tracksPlaying.seek(id);
+  };
 }
 
 const media_player = new MediaPlayer();
--- elements.js
+++ elements.js
@@ -1,22 +1,41 @@
 let songs = [
-  "Boyfriend.mp3",
-  "Catching-Feelings.mp3",
-  "Cold-Water.mp3",
-  "Forever.mp3",
-  "Freeze.mp3",
-  "I-Would.mp3",
-  "Out-Of-Town-Girl.mp3",
-  "Take-You.mp3",
-  "What-Do-You-Mean.mp3",
-  "Yellow-Raincoat.mp3",
+  ["Boyfriend.mp3", "Justin Bieber"],
+  ["Catching-Feelings.mp3", "Justin Bieber"],
+  ["Cold-Water.mp3", "Justin Bieber"],
+  ["Forever.mp3", "Justin Bieber"],
+  ["Freeze.mp3", "T Pain"],
+  ["I-Would.mp3", "Justin Bieber"],
+  ["Out-Of-Town-Girl.mp3", "Justin Bieber"],
+  ["Take-You.mp3", "Justin Bieber"],
+  ["What-Do-You-Mean.mp3", "Justin Bieber"],
+  ["Yellow-Raincoat.mp3", "Justin Bieber"],
 ];
 
 const play_pause = document.querySelector(".play");
 const play_prev = document.querySelector(".prev");
 const play_next = document.querySelector(".next");
+const song_name = document.querySelector(".name");
+const artist = document.querySelector(".artist");
 
 play_pause.addEventListener("click", () => media_player.playTheSong());
 
-play_prev.addEventListener("click", () => {});
+play_prev.addEventListener("click", () => media_player.playThePrevSong());
 
 play_next.addEventListener("click", () => media_player.playTheNextSong());
+
+song_name.textContent = `Unknown`;
+const displayName = (name, duration, artists) => {
+  song_name.innerHTML = `
+  <p>
+  <br/>  
+    Song: ${name.replace(".mp3", "").replace("-", " ")}
+    <br/><br/>
+    Duration: ${Math.ceil(duration / 60)}:00 min
+  </p>`;
+
+  artist.innerHTML = `
+    <p>
+      Artist: ${artists}
+    </p>
+  `;
+};
